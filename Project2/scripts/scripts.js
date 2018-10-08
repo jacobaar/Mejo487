e117d8d6a997511ba914f4fda808f25c7ae509a0
jacobaar <jacobaar@live.unc.edu>
2018-10-08 14:59:02 -0400

    Added more code and functionality.

--- Project2/scripts/scripts.js
+++ Project2/scripts/scripts.js
@@ -1,3 +1,27 @@
-$(document).ready(function(){
-  $('.slider').slick();
+var slider = $(document).ready(function(){
+  $('.slider').slick({
+	  infinite: false
+  });
 });
+
+//$('.slick-prev').hide();
+
+slider.on('afterChange', function(event, slick, currentSlide){
+	if (currentSlide === 0) {
+      $('.slick-prev').hide();
+      $('.slick-next').show();
+    }
+	else if (currentSlide === 11){
+	  $('.slick-prev').show();
+      $('.slick-next').hide();
+	}
+	else{
+	  $('.slick-prev').show();
+      $('.slick-next').show();
+	}
+});
+
+$(document).ready(function(){
+	$('.quizSlide').slickQuiz();
+});
+

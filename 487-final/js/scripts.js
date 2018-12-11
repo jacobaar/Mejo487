0233b4cd4f4901e687a828c1e9b3744f8ce4816c
jacobaar <jacobaar@live.unc.edu>
2018-12-11 17:27:31 -0500

    Finished the Brenda function

--- 487-final/js/scripts.js
+++ 487-final/js/scripts.js
@@ -1,8 +1,21 @@
 $(document).ready(function() {
   console.log('Ready to begin the test...');
+  
+  var student = {name:"Brenda", age:20, likesCoding:true};
+  
+  printBrenda(student);
+
 
   $('.mobile-btn').click(function(){
 	  $('#mobileNav').toggle();
   });
   
+  
 });
+
+function printBrenda(person){
+ if(person.likesCoding == true){
+	 console.log("Name: " + person.name);
+	 console.log("Age: " + person.age);
+ }
+}

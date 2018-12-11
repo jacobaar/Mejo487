9b6d33b8a1638cd6293877e921e6bf47551da843
jacobaar <jacobaar@live.unc.edu>
2018-12-11 18:45:08 -0500

    Looped through the JSON data

--- 487-final/js/scripts.js
+++ 487-final/js/scripts.js
@@ -10,6 +10,28 @@ $(document).ready(function() {
 	  $('#mobileNav').toggle();
   });
   
+  // var url = 'refugees.JSON';
+  
+  $.ajax({
+    type: 'GET',
+    url: 'js/refugees.json',
+    success: function(data){
+      console.log(data);
+      buildContent(data);
+    },
+    error: function(error){
+      console.log(error);
+    }
+  })
+  
+  function buildContent(data){
+	for(i=0; i< data.length; i++) {
+		console.log("Origin: " + data[i].Origin + " Value: " + data[i].Value);
+		$('.cards').append('<div class = "card card' + i + '"></div>');
+		$('.card' + i).append('<p class = "cardOrigin">' + data[i].Origin +'</p>');
+		$('.card' + i).append('<p class = "cardRequests">' + data[i].Value + '</p>');
+	}
+  }
   
 });
 
